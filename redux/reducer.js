feca85b0c67f30914efedcd38637a2693cbc7e00
Przemek <lprzemek@gmail.com>
2017-03-02 15:04:14 +0100

    delete, complete reducers

--- redux/reducer.js
+++ redux/reducer.js
@@ -5,6 +5,7 @@ function getNextId(state) {
 }
 
 const reducer =  function (state = {}, action) {
+  console.log(state);
   switch(action.type){
     case 'ADD_TODO':
       return Object.assign({}, state, {
@@ -13,6 +14,18 @@ const reducer =  function (state = {}, action) {
           text: action.payload,
           completed: false,
         }, ...state.todos]
+      });
+    case 'REMOVE_TODO':
+    return Object.assign({}, state, {
+      todos: state.todos.filter(todo =>  {
+        return  todo.id !== action.payload ? true : false
+      })
+    });
+    case 'COMPLETE_TODO':
+      return Object.assign({}, state, {
+        todos: state.todos.map(todo =>  {
+          return  todo.id !== action.payload ? todo : Object.assign({}, todo, {completed: true})
+        })
       });
     default:
       return state;
